9b0ed67f700527093bdd7099c70447516d6bd4e8
sshevya <37146723+sshevya@users.noreply.github.com>
2020-03-27 23:39:08 +0530

    Array | very imp |read only + inplace+linear

---
+++ repeating_no.cpp
@@ -0,0 +1,28 @@
+int Solution::repeatedNumber(const vector<int> &A) {
+    
+    //map<int,int> m;
+    int n=A.size();
+    int slow,fast;
+    slow=A[0];
+    fast=A[A[0]];
+    while(slow!=fast){
+        slow=A[slow];
+        fast=A[A[fast]];
+    }
+    fast=0;
+    while(slow!=fast){
+        slow=A[slow];
+        fast=A[fast];
+    }
+   /* map<int,int>::iterator i;
+    for(i=m.begin();i!=m.end();i++){
+        
+        if(i->second>1){
+            return i->first;
+        }
+    }
+   */ 
+    
+    return slow;
+    
+}
