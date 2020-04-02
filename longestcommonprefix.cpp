b26a3eca78f631653a7e76a2ee318a31b079640a
sshevya <37146723+sshevya@users.noreply.github.com>
2020-04-03 00:47:28 +0530

    String

---
+++ longestcommonprefix.cpp
@@ -0,0 +1,33 @@
+int min(int a, int b){
+    if(a<b){
+        return a;
+    }
+    return b;
+}
+
+string Solution::longestCommonPrefix(vector<string> &A) {
+    
+    
+    int n=A.size();
+    int min_l=A[0].length();
+    for(int i=0;i<n;i++){
+        min_l=min(min_l,A[i].length());
+    }
+    string lcp=A[0];
+    int count=0,i,j;
+    for(i=1;i<n;i++){
+        count=0;
+        string s="";
+        for(j=0;j<min_l;j++){
+           if(A[i-1][j]==A[i][j]){
+               count++;
+               s+=A[i][j];
+           } 
+        }
+        min_l=count;
+        lcp=s;
+    }
+    
+    
+    return lcp;
+}
