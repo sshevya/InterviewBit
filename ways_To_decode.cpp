99885ede966b06b269ec6f377a87358e296ef61e
sshevya <37146723+sshevya@users.noreply.github.com>
2020-07-28 21:34:23 +0530

    DP | longint

--- ways_To_decode.cpp
+++ ways_To_decode.cpp
@@ -1,31 +1,23 @@
 int Solution::numDecodings(string A) {
-    
     int n=A.length();
     if(A[0]=='0'){
         return 0;
     }
-    int dp[n+1]={0};
-    
+    long long int dp[n+1]={0};
     dp[0]=1;
     dp[1]=1;
-    
     for(int i=2;i<=n;i++){
         dp[i]=0;
-        if(A[i-1]>'0'){
+         if(A[i-1]>'0' ){
             dp[i]=dp[i-1];
         }
-        if(A[i-2]=='1' || A[i-2]=='2' && A[i-1]<'7' ){
-            dp[i]+=dp[i-2];
+        if((A[i-2]=='1' && A[i-1]<='9') || (A[i-2]== '2' && A[i-1]<'7') ){
+            dp[i]=(dp[i]+dp[i-2])%1000000007;
         }
-        //cout<<A[i-]<<dp[i];
+       
+        
+        
     }
     
-    
-    
-    
-    
-    
-    
-    return dp[n];
-    
+    return dp[n]%1000000007;
 }
