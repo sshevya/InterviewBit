e78177dc7afa6e73880c6e96fb91a94347f0eed3
sshevya <37146723+sshevya@users.noreply.github.com>
2020-07-31 02:09:33 +0530

    DP

--- maxsum_noadjacent.cpp
+++ maxsum_noadjacent.cpp
@@ -3,7 +3,21 @@
 
 int Solution::adjacent(vector<vector<int> > &A) {
     int n=A[0].size();
+    /**
+    DP Sol
     
+    int dp[n]={0};
+    dp[0]=max(A[0][0],A[1][0]);
+
+    dp[1]=max(dp[0],max(A[0][1],A[1][1]));
+    dp[2]=max(dp[0]+max(A[0][2],A[1][2]),dp[1]);
+    for(int i=2;i<n;i++){
+        dp[i]=max(dp[i-2]+max(A[0][i],A[1][i]),dp[i-1]);
+    }
+    
+    return dp[n-1];
+
+**/
     int incl_sum=max(A[0][0],A[1][0]);
     int excl_sum=0, excl_new;
 
