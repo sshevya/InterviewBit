137544df51fb11f4887b10756cfd08314fde824f
sshevya <37146723+sshevya@users.noreply.github.com>
2020-08-07 00:51:12 +0530

    DP | Vimp

---
+++ RegEx2.cpp
@@ -0,0 +1,36 @@
+int Solution::isMatch(const string A, const string B) {
+    
+    int n=A.length();
+    int m=B.length();
+    vector<vector<bool>> dp(n+1,vector<bool>(m+1,0));
+    
+    dp[0][0]=1;
+    for(int i=2;i<m+1;i++){
+        if(B[i-1]=='*'){
+            dp[0][i]=dp[0][i-2];
+        }
+        /*else if(B[i-1]=='.'){
+            dp[0][i]=dp[0][i-1];
+        }*/
+    }
+    
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<m+1;j++){
+            if(B[j-1]==A[i-1]||B[j-1]=='.'){
+                dp[i][j]=dp[i-1][j-1];
+            }else if(B[j-1]=='*'){
+                
+                    dp[i][j]=dp[i][j-2];
+               if(B[j-2]==A[i-1]|| B[j-2]=='.'){
+                   dp[i][j]=dp[i][j]||dp[i-1][j];
+               }
+                
+            }else{
+                dp[i][j]=0;
+            }
+        }
+    }
+    
+    
+    return dp[n][m];
+}
