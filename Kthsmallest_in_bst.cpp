a72f4eac62d7329f2c2481f1d713999470613c28
sshevya <37146723+sshevya@users.noreply.github.com>
2020-08-07 14:54:19 +0530

    Tree

--- Kthsmallest_in_bst.cpp
+++ Kthsmallest_in_bst.cpp
@@ -7,6 +7,36 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+////First method
+ 
+void inorder(TreeNode* A, vector<int> & v, int B,int& ans){
+    if(A==NULL){
+        return;
+    }
+    inorder(A->left,v,B,ans);
+    v.push_back(A->val);
+    if(v.size()==B){
+        ans=v[B-1];
+        return;
+    }
+    inorder(A->right,v,B,ans);
+    
+}
+ 
+int Solution::kthsmallest(TreeNode* A, int B) {
+    
+    if(A==NULL){
+        return 0;
+    }
+    int p=0;
+    vector<int> v;
+    
+    
+    int ans=-1;
+    inorder(A,v, B, ans);
+    return ans;       
+}
+///Second Method
  
  //pass value of count with address otherwise error
 TreeNode* rec(TreeNode* A, int B, int& count){
