f8d99a56fc20975e82b8fbeb5eeb60e55aa0358c
sshevya <37146723+sshevya@users.noreply.github.com>
2020-08-07 23:25:43 +0530

    Tree | See concept

---
+++ ValidBST.cpp
@@ -0,0 +1,19 @@
+int Solution::solve(vector<int> &A) {
+    
+    stack<int> s;
+    int root=INT_MIN;
+    for(int i=0;i<A.size();i++){
+        
+        if(A[i]<root){
+            return 0;
+        }
+        
+        while(!s.empty() && s.top()<A[i]){
+            root=s.top();
+            s.pop();
+        }
+        s.push(A[i]);
+    }
+    return 1;
+    
+}
