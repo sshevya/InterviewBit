137a0e0396f2b4163211067f536fe34e146c98cc
sshevya <37146723+sshevya@users.noreply.github.com>
2020-08-13 00:53:28 +0530

    Bit Manipulation | 1 line concept

--- no_of_1bit.cpp
+++ no_of_1bit.cpp
@@ -1,15 +1,17 @@
 int Solution::numSetBits(unsigned int A) {
-    int c = 0;
-    /*while(A)  //Simple O(log n) approach
-    {
-        if (A & 1)
-            ++c;
-        A >>= 1;
-    }*/
-    while(A!=0) //Brian Kernighan’s Algorithm: iterates only over set bits
-    {
-        A &= A-1;
-        ++c;
+    
+    /****Subtracting one from binary number flips all digits after rightmost 1 including 1, 
+     * so & of n, n-1 will unset rightmost set bit, no of times this happeens is number of 1*****/
+    //Brian Kernighan;'s algo
+    int count =0;
+    while(A!=0){
+        A=A&(A-1);
+        count++;
     }
-    return c;    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+
+    
+    return count;
+    
+    
 }
