e4625debde6c28ea6b8c9b1feff80de06137c968
sshevya <37146723+sshevya@users.noreply.github.com>
2021-04-16 20:17:49 +0530

    array

---
+++ minStepsInInfiniteGrid.cpp
@@ -0,0 +1,15 @@
+int Solution::coverPoints(vector<int> &A, vector<int> &B) {
+    
+    
+    int steps =0;
+    int n=A.size();
+    if(n==1){
+        return 1;
+    }
+    for(int i=1;i<n;i++){
+        
+        steps
+        
+        
+    }
+}
