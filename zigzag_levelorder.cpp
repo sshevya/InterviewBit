740f5f323684fa5c2eab0aa6aafb81f3e183665d
sshevya <37146723+sshevya@users.noreply.github.com>
2021-04-23 13:43:34 +0530

    See all efficient methods

--- zigzag_levelorder.cpp
+++ zigzag_levelorder.cpp
@@ -65,7 +65,61 @@ vector<vector<int> > Solution::zigzagLevelOrder(TreeNode* A) {
     
 }
 
+
+
+/***
+BFS based solution, uses more memory
+vector<vector<int> > Solution::zigzagLevelOrder(TreeNode* A) {
+    
+     vector<vector<int>> v;
+   map<int, vector<int>> m;
+    if(A==NULL){
+        return v;
+    }
+    
+    queue<pair<TreeNode*, int>> q;
+    q.push(make_pair(A,0));
+    
+    while(!q.empty()){
+        
+        TreeNode* top = q.front().first;
+        int l = q.front().second;
+        q.pop();
+        m[l].push_back(top->val);
+        
+        if(top->left){
+            q.push(make_pair(top->left, l+1));
+        }
+        
+        if(top->right){
+            q.push(make_pair(top->right, l+1));
+        }
+        
+        
+    }
+    int j=0;
+    
+    for(auto i = m.begin();i!=m.end();i++){
+        vector<int> temp = i->second;
+        if(j%2==0){
+            v.push_back(temp);
+        }else{
+            reverse(temp.begin(), temp.end());
+            v.push_back(temp);
+        }
+        j++;
+    }
+    
+    
+    return v;
+}
+
+
+
+**///
 /*
+
+Third solution
 void zig(TreeNode* A, vector<vector<int> >& sol, bool left, int level){
     if(A == NULL){
         return;
