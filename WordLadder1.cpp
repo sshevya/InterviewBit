d66f188e1a19b831c5155541d4aa5633436dac7b
sshevya <37146723+sshevya@users.noreply.github.com>
2021-04-27 19:26:59 +0530

    Graph | Multiple concepts | multiple sol | bidirectional bfs

--- WordLadder1.cpp
+++ WordLadder1.cpp
@@ -1,3 +1,60 @@
+/**** 
+Solution 1
+Multiple Concepts
+don't use map, use set, else TLE
+keep erasing from set because worst case time for find is linear in set
+***/
+int Solution::solve(string A, string B, vector<string> &C) {
+    if(A==B){
+        return 0;
+    }
+    
+    unordered_set<string> m;
+    for(auto x: C){
+        m.insert(x);
+    }
+    
+    queue<pair<string,int>> q;
+    q.push({A,1});
+    
+    while(!q.empty()){
+        
+            string temp = q.front().first;
+            int h =q.front().second;
+            q.pop();
+            if(temp == B){
+                return h;
+            }
+            for(int i =0;i<temp.size();i++){
+                string s = temp;
+                for(char j = 'a'; j<='z'; j++){
+                    s[i]=j;
+                    
+                    if(s==temp){
+                        continue;
+                    }
+                    if(s==B){
+                        return h+1;
+                    }
+                    
+                    if(m.find(s)!=m.end()){
+                        q.push({s,h+1});
+                        m.erase(s);
+                    }
+                }
+                
+            }
+            
+        
+    }
+    return 0;
+    
+}
+
+/**** See bidirectional BFS implementatoion  & concept*****/
+
+
+/********** TLE  Solution(2) *****/
 bool isAdj(string A, string B){
     int count=0;
     int n=A.length();
