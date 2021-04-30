f925c35657166d7459605de3f674574a0f22a08a
sshevya <37146723+sshevya@users.noreply.github.com>
2021-04-30 14:11:14 +0530

    3D dp | Vimp | see explanation

--- ScrambleString.cpp
+++ ScrambleString.cpp
@@ -17,12 +17,24 @@ int Solution::isScramble(const string A, const string B) {
     }
     
     vector<vector<vector<bool>>> dp(n,vector<vector<bool>>(n,vector<bool>(n,0)));
-    
+    /*
+    Starting at i in A and j in b substring of length 0 is scrambled
+    */
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             dp[i][j][0]=(A[i]==B[j]);
         }
     }
+    /*
+    Starting at index i in A and j in B a substring of length k is scrambled if for any p 
+    starting at i substring of length p is scrambled with substring of length p starting at j in B 
+    and substring of length k-p-1 starting at i+p+1 in A is scrambled with substring starting at j+p+1 in B
+    OR
+     starting at i substring of length p is scrambled with substring of length p starting at j+k-p in B 
+    and substring of length k-p-1 starting at i+p+1 in A is scrambled with substring starting at j in B
+    
+    
+    */
     
     for(int k=1;k<n;k++){
         for(int i=0;i<n-k;i++){
